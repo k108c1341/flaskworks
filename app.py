1b8bcc44f400f3b2f4d23b62f62a797fc56ada8b
k108c1341 <k018c1341@it-neec.jp>
2020-07-27 13:10:41 +0900

    6.2.image_upload( sec )

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,8 +16,12 @@ def index():
 @app.route('/send', methods=['POST'])
 def send():
     img_file = request.files['img_file']
-    img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
-    return '<p>画像' + img_file.filename + 'を送信しました</p>'
+    if img_file and allowed_file(img_file.filename):
+        img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
+        return '<p>画像' + img_file.filename + 'を送信しました</p>'
+    else:
+        return '<p>許可されていない拡張子です</p>'
+
 if __name__=='__main__':
     app.debug = True
     app.run()
