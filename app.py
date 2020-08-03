155256d37ed4ca09e559e1f4766b0c0dfbe33338
k108c1341 <k018c1341@it-neec.jp>
2020-08-03 11:31:06 +0900

    7.1.session

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './static/uploads/'
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/send', methods=['POST'])
-def send():
-    img_file = request.files['img_file']
-    if img_file and allowed_file(img_file.filename):
-        img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
-        return '<p>画像' + img_file.filename + 'を送信しました</p>'
+    if 'username' in session:
+        return render_template('index.html', username=str(session['username']))
     else:
-        return '<p>許可されていない拡張子です</p>'
+        return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form.get('username'):
+        session['username'] = request.form.get('username')
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return render_template('login.html')
 
 if __name__=='__main__':
     app.debug = True
