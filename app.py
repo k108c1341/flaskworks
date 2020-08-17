bb389b7426b0a2b1d35d8290795d58c6ebd35203
k108c1341 <k018c1341@it-neec.jp>
2020-08-17 12:12:23 +0900

    9.2.database(select)

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = './static/uploads'
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
     conn = db.connect(**db_param)
