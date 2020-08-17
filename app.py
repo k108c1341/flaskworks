1debc2cf45b0ca5c7bc9cdf881151eda1e95edc2
k108c1341 <k018c1341@it-neec.jp>
2020-08-17 12:35:29 +0900

    9.3.database(update)

--- app.py
+++ app.py
@@ -28,23 +28,27 @@ def index():
     cur.close()
     conn.close()
     return render_template('index.html', list=rows)
-
 @app.route('/send', methods=['POST'])
 def send():
+    print(request.files)
     title = request.form.get('title')
     price = request.form.get('price')
-    if title=="" or price=="":
+    image = request.files['img_file']
+    if title=="" or price=="" or image=="":
         return redirect('/')
+
+    if image and allowed_file(image.filename):
+        image.save('static/uploads/' + image.filename)
     conn = db.connect(**db_param)
     cur = conn.cursor()
 
-    stmt = 'SELECT * FROM books WHERE title=%s'
+    stmt = 'SELECT * FROM list WHERE title=%s'
     cur.execute(stmt, (title,))
     rows = cur.fetchall()
     if len(rows)==0:
-        cur.execute('INSERT INTO books(title, price) VALUES(%s, %s)', (title, int(price)))
+        cur.execute('INSERT INTO list(title, price, image) VALUES(%s, %s, %s)', (title, int(price), image.filename))
     else:
-        cur.execute('UPDATE books SET price=%s WHERE title=%s', (int(price), title))
+        cur.execute('UPDATE list SET price=%s, image=%s WHERE title=%s', (int(price), image.filename, title))
     conn.commit()
     cur.close()
     conn.close()
